30b8c353d0820fe8501c7005315628a953bb293d
Slanecznik <vova.shigaev121183@gmail.com>
2025-10-17 23:14:38 +0200

    firstcommit

--- lesson-06/02.js
+++ lesson-06/02.js
@@ -24,4 +24,22 @@ const doubledNumbers = map(numbers, (element, index) => {
 console.log(doubledNumbers) // Должен вывести: [2, 4, 6, 8, 10]
 */
 
-const map = () => {}
+const map = (array, callback) => {
+    const result = []
+
+    for (let i = 0; i < array.length; i++) {
+        const element = array[i]
+        const newValue = callback(element, i)
+        result.push(newValue)
+    }
+
+    return result
+}
+
+const numbers = [1, 2, 3, 4, 5]
+
+const doubledNumbers = map(numbers, (element, index) => {
+    return element * 2
+})
+
+console.log(doubledNumbers)
