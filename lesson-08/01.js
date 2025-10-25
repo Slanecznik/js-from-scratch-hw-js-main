263be36ea7c6a0a69998ef951c24f357024b284d
Slanecznik <vova.shigaev121183@gmail.com>
2025-10-26 00:12:34 +0200

    firstCommit

--- lesson-08/01.js
+++ lesson-08/01.js
@@ -6,4 +6,14 @@
   Другой вариант - использовать метод split строк, и метод массива join.
 */
 
-function capitalizeWords() {}
+
+function capitalizeWords(str) {
+    return str
+        .split(' ')
+        .map(word => word[0].toUpperCase() + word.slice(1))
+        .join(' ');
+}
+
+console.log(capitalizeWords("hello world from javascript"));
+
+
